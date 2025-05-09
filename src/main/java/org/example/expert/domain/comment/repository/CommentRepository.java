5a287158f0d0773557a49da243dbae52c8fb44c3
J <justonesip82@gmail.com>
2025-05-09 16:35:38 +0900

    refactor: Lv2-7 N+1

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    // JOIN -> JOIN FETCH로 바꿔 user까지 한번에 가져올 수 있도록 ! => N+1 해결 ..~
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
